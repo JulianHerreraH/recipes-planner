f527b5356ef9c87a79e3cf65b3400edbd0175c24
Julian H <cjulianh46@gmail.com>
2020-09-23 18:58:25 -0600

    Navbar mobile

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,16 +1,33 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { NavLink } from "react-router-dom";
 import { ThemeContext } from "../context/ThemeContext";
 
+
 function Navbar(props) {
+  const [isNavOpen, setNav] = useState(false)
   const context = useContext(ThemeContext);
   const theme = context.isLightTheme ? context.navLight : context.navDark;
+  const navClass = isNavOpen ? "is-active" : "";
+
   return (
-    <nav className={`${theme} navbar round-border`}>
+    <nav className={`${theme} navbar round-border`} role="navigation" aria-label="main navigation">
       <div className="navbar-brand">
         <h1 className="navbar-item is-size-4">Recipe Planner</h1>
+        <div 
+          onClick={() => setNav(!isNavOpen)} 
+          role="button" 
+          className={`navbar-burger ${navClass}`} 
+          data-target="navMenu" 
+          aria-label="menu" 
+          aria-expanded="false"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </div>
       </div>
-      <div className="navbar-menu">
+
+      <div id="navMenu" className={`navbar-menu ${navClass}`}>
         <div className="navbar-start">
           <NavLink
             exact
@@ -37,7 +54,7 @@ function Navbar(props) {
         </div>
         <div className="navbar-end">
           <div className="navbar-item">
-            <button className={`${theme} button  is-light is-rounded`}>
+            <button className={`button is-danger is-rounded`}>
               New
             </button>
           </div>
